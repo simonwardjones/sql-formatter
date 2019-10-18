661e9fe1b35f497385ef5f7cc5e32dc29ab3cfff
simonwardjones <simonwardjones@yahoo.co.uk>
2019-10-18 10:21:40 +0100

    update the demo

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,7 @@
-import { DefaultTokenizerConfig } from './tokenizer_config'
 import { Tokenizer } from './tokenizer'
+import { DefaultTokenizerConfig } from './tokenizer_config'
+import { TokenFormatter } from './layout'
+import { DefaultLayoutConfig } from './layout_config'
 
 export function hello(): string {
   const world = '🦓';
@@ -9,24 +11,28 @@ export function hello(): string {
 
 function listCurrentDefaultTokens() {
   console.log('Hello from ts, listing token kinds')
-  new Tokenizer(DefaultTokenizerConfig).tokenTypes.forEach(x => console.log(x.kind))
+  new Tokenizer(DefaultTokenizerConfig).tokenTypes.forEach(x => console.log(x.name))
 }
 
 // const sqlExample = '$current_date select'
 
 const sqlExample = `
-select T.column1 as col, 
-count(*),
-current_date::date,
-489c ehr hw.';.[.\[.\[;/.'.\.]]]
+select 
+current_date::date, T.col1-T2.col1,
+row_number() over (partition by t2.col2),
+count(*) as col2,
+count(*) as count,
+/* test the 
+block */
 --fsd here is a comment
 // and another -- lol
-col2 from demo_sb.table T
-inner join "another"."fake"."example"
+from database.table T
+inner join database.table2 T2 on T.col1 = T2.col2
+inner join "another"."fake"."example" XST on T.col1 = XST.name
 where 1=1
 and x >= 32
-where 'this is a
--- string,'
+group by 1,2,3,4,5
+having count < 120
 `
 
 function addExampleToBody(): void {
@@ -40,8 +46,13 @@ function exampleDebug() {
   var divideBy = "#"
   console.log('debugging' + '\n' + divideBy.repeat(40) + '\n')
   var demoTokenizer = new Tokenizer(DefaultTokenizerConfig)
+  var demoTokenFormatter = new TokenFormatter(DefaultLayoutConfig)
   console.log(demoTokenizer.tokens)
-  console.log(demoTokenizer.tokenize(sqlExample))
-
+  var tokens = demoTokenizer.tokenize(sqlExample)
+  // console.log(tokens)
+  console.log(demoTokenFormatter.formatTokens(tokens))
 }
 exampleDebug()
+
+// let fix = /^'(?:(?=([^\\']*))\1(?:[\\].|'')?)*'|^"(?:(?=([^\\"]*))\2(?:[\\].)?)*"|^\$\$.*?\$\$/
+// this is a very efficient fix to back tracking
