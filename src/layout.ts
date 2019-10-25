f1f881e8ea08e795992cd6f7ddb94c9d90bff9c2
simonwardjones <simon.ward-jones@carwow.co.uk>
2019-10-25 19:15:02 +0100

    writing reserved words start

--- src/layout.ts
+++ src/layout.ts
@@ -1,14 +1,10 @@
 import { LayoutConfig } from './layout_config'
 import { Token, tokenNames } from './tokenizer'
 
-// export enum stateFlag {
-//     AWAITING_START = 'AWAITING_START',
-//     SELECT = "SELECT_CONTEXT"
-// }
-
 export interface State {
     contextStack: Context[]
     contextDepth: number
+    previousNonWhitespaceToken: Token | null
 }
 
 export interface Context {
@@ -38,7 +34,8 @@ export class TokenFormatter {
         this.config = config
         this.state = {
             contextStack: [globalContext],
-            contextDepth: 0
+            contextDepth: 0,
+            previousNonWhitespaceToken: null
         }
         this.formattedQuery = ''
         this.stacks = {}
@@ -54,7 +51,8 @@ export class TokenFormatter {
     reset() {
         this.state = {
             contextStack: [globalContext],
-            contextDepth: 0
+            contextDepth: 0,
+            previousNonWhitespaceToken: null
         }
         this.stacks = {}
     }
@@ -70,43 +68,45 @@ export class TokenFormatter {
                 console.log('Error encountered writing tokens without formating')
                 return tokens.map(token => token.value).join('')
             }
-            this.updateContext(token)
             if (this.tokenRequiresStack(token)) {
                 console.log('hunting for end with line length character allowance')
             }
-            this.formattedQuery += this.writeToken(token)
+            this.writeToken(token)
+            this.updateContext(token)
+            this.updateState(token)
         }
         return this.formattedQuery
     }
 
     updateContext(token: Token) {
         for (let context of this.contexts) {
-            if (token.value == context.contextStart) {
+            if (token.value === context.contextStart) {
                 console.log(`Adding context to stack ${context.name}`)
                 this.state.contextDepth += 1
                 this.state.contextStack.push(context)
             }
-            else if (token.value == context.contextEnd && context.name != this.currentContext().name) {
+            else if (token.value === context.contextEnd && context.name != this.currentContext().name) {
                 console.log(`WARNING - trying to open ${token.value} context before closing ${this.currentContext().name} context`)
                 this.error = 1
             }
-            else if (token.value == context.contextEnd) {
+            else if (token.value === context.contextEnd) {
                 console.log(`Closing context ${context.name}`)
                 this.state.contextStack.pop()
             }
         }
     }
-
     tokenRequiresStack(token: Token) {
         return false
     }
-    writeToken(token: Token): string {
+    updateState(token: Token) {
+        if (!(token.name === 'whitespace')) {
+            this.state.previousNonWhitespaceToken = token
+        }
+    }
+    writeToken(token: Token) {
         switch (token.name) {
             case tokenNames.RESERVED_WORDS:
-                if (this.config.newLineReservedWords.includes(token.value)) {
-                    return token.value + '\n'
-                }
-                return token.value
+                this.writeReservedWord(token)
             case tokenNames.STRING:
                 return token.value
             case tokenNames.BLOCK_COMMENT:
@@ -133,6 +133,19 @@ export class TokenFormatter {
                 return token.value
             case tokenNames.ERROR_TOKEN:
                 return token.value
+        }
+    }
+    writeReservedWord(token: Token) {
+        if (this.currentContext.name === 'GLOBAL_CONTEXT' || !this.state.previousNonWhitespaceToken) {
+            this.formattedQuery += token.value + '\n'
+        }
+        else if (
+            this.config.newLineReservedWords.includes(token.value) &&
+            !(this.config.newLineReservedWords.includes(this.state.previousNonWhitespaceToken.value))
+        ) {
+            this.formattedQuery += '\n' + token.value + ' '
+        } else {
+            this.formattedQuery += token.value + ' '
         }
     }
 }
