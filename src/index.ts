46fb26ea07362545d5e6e1b517ad82fa528fb826
simonwardjones <simon.ward-jones@carwow.co.uk>
2019-11-07 13:46:35 +0000

    basic toplevel class

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Tokenizer } from './tokenizer'
 export { DefaultTokenizerConfig } from './tokenizer_config'
 export { TokenFormatter } from "./layout"
 export { DefaultLayoutConfig } from './layout_config'
+export { SqlFormatter } from './sql_formatter'
---
+++ src/sql_formatter.ts
@@ -0,0 +1,13 @@
+import { Tokenizer } from './tokenizer'
+import { DefaultTokenizerConfig, TokenizerConfig } from './tokenizer_config'
+import { TokenFormatter } from './layout'
+import { DefaultLayoutConfig } from './layout_config'
+
+export class SqlFormatter {
+    tokenizer = new Tokenizer(DefaultTokenizerConfig)
+    tokenFormatter  = new TokenFormatter(DefaultLayoutConfig)
+    format(query: string) {
+        let tokens = this.tokenizer.tokenize(query)
+        return this.tokenFormatter.formatTokens(tokens)
+    }
+}
